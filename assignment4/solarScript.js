b748d99669ad8caf805a43959cff73a18027481b
asiemering <alison.siemering@gmail.com>
2022-07-14 01:30:07 -0700

    update animations

--- assignment4/solarScript.js
+++ assignment4/solarScript.js
@@ -1,16 +1,72 @@
 
 $(document).ready(function(){
-  $(".animation").hide();
-  $("button").click(function(){
-    $(".animation").fadeIn();
-    $(".animation").animate({
-      height: '+=150px',
-      width: '+=150px'
+
+  $(".maroonAnimation").hide();
+  $(".redAnimation").hide();
+  $(".orangeAnimation").hide();
+  $(".yellowAnimation").hide();
+  $(".greenAnimation").hide();
+  $(".lightblueAnimation").hide();
+  $(".blueAnimation").hide();
+  $(".darkblueAnimation").hide();
+  $(".purpleAnimation").hide();
+
+  $(".mercuryButton").click(function(){
+
+    $(".maroonAnimation").show();
+
+    $(".maroonAnimation").animate({
+      height: '80%',
+      width: '100%',
+      top: '0',
+      left: '0'
     });
 
-    $(".animation").animate({
-      height: '-=100px',
-      width: '-=100px'
-    });
   });
+
+  $(".venusButton").click(function(){
+
+    $(".blueAnimation").show();
+
+    $(".blueAnimation").animate({
+      height: '80%',
+      width: '100%',
+      top: '0',
+      left: '0'
+    });
+
+  });
+
+  $(".maroonAnimation").mouseleave(function(){
+
+    $(".maroonAnimation").animate({
+      height: '10%',
+      width: '15%',
+      top: '30%',
+      left: '40%'
+    });
+
+    $(".maroonAnimation").hide();
+
+  });
+
+  $(".blueAnimation").mouseleave(function(){
+
+    $(".blueAnimation").animate({
+      height: '10%',
+      width: '15%',
+      top: '30%',
+      left: '40%'
+    });
+
+    $(".blueAnimation").hide();
+
+  });
+
 });
+
+
+
+
+
+
