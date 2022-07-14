810a34b602e0986030f84fc7124647566f82af93
asiemering <alison.siemering@gmail.com>
2022-07-13 23:50:36 -0700

    solarScript.js animation test

--- assignment4/solarScript.js
+++ assignment4/solarScript.js
@@ -1,9 +1,16 @@
 
 $(document).ready(function(){
+  $(".animation").hide();
   $("button").click(function(){
-    $("div").animate({
+    $(".animation").fadeIn();
+    $(".animation").animate({
       height: '+=150px',
       width: '+=150px'
+    });
+
+    $(".animation").animate({
+      height: '-=100px',
+      width: '-=100px'
     });
   });
 });
